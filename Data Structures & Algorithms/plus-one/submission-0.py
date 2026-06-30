0f5397f0f82c1ff5702491a6b7000e24beee2258
Parth Borkute <parthborkute23@gmail.com>
2026-06-30 22:36:13 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = int("".join(map(str, digits)))
+        num = num + 1
+
+        res = [int(d) for d in str(num)]
+        return res
+
+ 
+        
